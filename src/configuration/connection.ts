0e26790e1a718aa8732b477ef9eb26bea170d188
Ahsan Khan <ahsenhameed381@gmail.com>
2023-03-30 18:00:17 +0500

    Remove DB url and add in Readme.md

--- src/configuration/connection.ts
+++ src/configuration/connection.ts
@@ -1,4 +1,4 @@
 export const mongoDBUrl: string =
-  "mongodb+srv://ahsenhameed381:Shine&f9@cluster0.9q2ev15.mongodb.net/test";
+  "";
 export const jwtSecret: string =
   "YWQ3NTQ5MjYtNjg3Yi00YzdjLTg1OTgtM2I3YmZhZjA2ZWNi";
